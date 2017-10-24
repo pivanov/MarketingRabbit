57eb378f504b727d9f06756b61425a6c0c31c407
Omar Torres <01omartorres@gmail.com>
2017-10-24 15:30:37 -0400

    Edit initial ajax calls

--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -1,20 +1,21 @@
 export const signup = () => {
   return $.ajax({
     method: "POST",
-    url: 'api/users'
+    url: '/api/users'
   })
 }
 
 export const login = (user) => {
   return $.ajax({
     method: "POST",
-    url: "api/session"
+    url: "/api/session",
+    data: {user}
   })
 }
 
 export const logout = () => {
   return $.ajax({
     method: "DELETE",
-    url: "api/session"
+    url: "/api/session"
   })
 }
